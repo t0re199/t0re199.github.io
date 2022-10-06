8f23b63ffa9edfe0d2b3cd90007e424d3f9206f3
t0re199 <t0re199@t0re199-MBP.local>
2022-10-06 16:25:47 +0200

    Portfolio-item preview disabled

--- assets/js/main.js
+++ assets/js/main.js
@@ -180,9 +180,6 @@
         portfolioIsotope.arrange({
           filter: this.getAttribute('data-filter')
         });
-        portfolioIsotope.on('arrangeComplete', function() {
-          AOS.refresh()
-        });
       }, true);
     }
 
