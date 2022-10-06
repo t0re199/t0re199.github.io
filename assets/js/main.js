505ed60be5e9fa62752304ff4f7730f48947dcee
t0re199 <t0re199@t0re199-MBP.local>
2022-10-06 16:04:29 +0200

    Portfolio items filter added

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,15 +162,15 @@
    * Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
+    let portfolioContainer = select('.spportfolio-container');
     if (portfolioContainer) {
       let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item'
+        itemSelector: '.spportfolio-item'
       });
 
-      let portfolioFilters = select('#portfolio-flters li', true);
+      let portfolioFilters = select('#spportfolio-flters li', true);
 
-      on('click', '#portfolio-flters li', function(e) {
+      on('click', '#spportfolio-flters li', function(e) {
         e.preventDefault();
         portfolioFilters.forEach(function(el) {
           el.classList.remove('filter-active');
